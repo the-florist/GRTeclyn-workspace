f6585285f7c98bc7c593ebef199a3706e7dbbd9b
the-florist <eaf49@cam.ac.uk>
2024-12-30 09:11:54 +0000

    Mid change, but need to make a private repo

--- Source/InitialConditions/ScalarFields/RandomField.hpp
+++ Source/InitialConditions/ScalarFields/RandomField.hpp
@@ -7,13 +7,14 @@
 #define RANDOMFIELD_HPP_
 
 #include "Cell.hpp"
-#include "Tensor.hpp"
-#include "UserVariables.hpp"
 #include "InitialScalarData.hpp"
-#include "Coordinates.hpp"
 #include "VarsTools.hpp"
-//#include <random> // needed for random number generator
 #include <fstream>
+
+#include <AMReX_MultiFab.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_FFT.H>
+#include <AMReX_Random.H>
 
 //! Class to create a Gaussian random field, 
 //! originally created for 2 massless tensor polarisation fields
@@ -24,22 +25,25 @@ class RandomField
         //! A structure for storing parameters essential to this class
         struct params_t 
         {
-            double num_fields; //!< Number of fields to generate
+            int num_scalar_fields; //!< Number of fields to generate
+            int calc_tensor_field; //!< Determines whether tensor perts are calculated
+
             double L;          //!< Length of the box
             double A;          //!< Amplitude factor (for basic tests)
             int which_seed;    //!< Which random seed will be chosen (defunct?)
-            int Nf;            //!< Fine resolution to downsample from, 
+
+            int N_readin;      //!< used to read in the private N variable
+            int N_fine;            //!< Fine resolution to downsample from, 
                                 //! used for convergence testing
         };
 
-        RandomField(params_t a_params, InitialScalarData::params_t a_background_params, 
+        RandomField(params_t a_params, InitialBackgroundData::params_t a_background_params, 
                      std::string a_spec_type)
                 : m_params(a_params), m_background_params(a_background_params), 
                   m_spec_type(a_spec_type)
         {
         }
 
-        template <class data_t>
         void init_random_field() const;
         
     private:
@@ -47,9 +51,9 @@ class RandomField
 
     protected:
         const params_t m_params;
-        const InitialScalarData::params_t m_background_params;
+        const InitialBackgroundData::params_t m_background_params;
         const std::string m_spec_type;
-}
+};
 
 
 #endif /* RANDOMFIELD_HPP_ */
--- Source/InitialConditions/ScalarFields/RandomField.impl.hpp
+++ Source/InitialConditions/ScalarFields/RandomField.impl.hpp
@@ -13,6 +13,21 @@
 
 template <class data_t> void RandomField::init_random_field() const
 {
+    BL_PROFILE("RandomField::init_random_field");
+    N = m_params.N_readin;
+
+    // Set up the problem domain and R2C fft 
+    amrex::IntVect domain_low(0, 0, 0);
+    amrex::IntVect domain_high(N-1, N-1, N/2);
+    amrex::Box domain(domain_low, domain_high);
+    //amrex::FFT::R2C random_field_fft(domain);
+
+    // Set up the arrays to store the field data sets
+    amrex::BoxArray ba(domain);
+    amrex::DistributionMapping dm(ba);
+    amrex::MultiFab hs_k(ba, dm, 2, 0);
+    amrex::MultiFab hij_k(ba, dm, 6, 0);
+    amrex::MultiFab hij_x(ba, dm, 6, 0);
 }
 
 #endif /* RANDOMFIELD_IMPL_HPP_*/
