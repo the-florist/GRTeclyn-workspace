d50277fde2a94d6cd476a8ee998711d6d93cb403
Ericka <eaf49@cam.ac.uk>
2026-04-16 12:23:18 +0100

    Create SecondOrderDerivatives hpp file

---
+++ Source/BoxUtils/SecondOrderDerivatives.hpp
@@ -0,0 +1,20 @@
+/* GRTeclyn
+ * Copyright 2022 The GRTL collaboration.
+ * Please refer to LICENSE in GRTeclyn's root directory.
+ */
+
+#ifndef SECONDORDERDERIVATIVES_HPP_
+#define SECONDORDERDERIVATIVES_HPP_
+
+#include "Cell.hpp"
+#include "DimensionDefinitions.hpp"
+#include "Tensor.hpp"
+#include "simd.hpp"
+#include <array>
+
+class SecondOrderDerivatives
+{
+
+};
+
+#endif /* SECONDORDERDERIVATIVES__HPP__ */
