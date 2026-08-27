fd755ec4100d7a228866af1c95a2e80c516445e6
Ericka Florio <eaf49@cam.ac.uk>
2026-08-27 12:45:22 +0100

    Add more Gpu::StreamSynchronise calls after ParallelFor

--- Examples/ScalarField/ScalarFieldLevel.cpp
+++ Examples/ScalarField/ScalarFieldLevel.cpp
@@ -101,6 +101,7 @@ void ScalarFieldLevel::specificAdvance()
                            TraceARemoval()(cell);
                            PositiveChiAndAlpha(simParams().min_chi, simParams().min_lapse)(cell);
                        });
+    Gpu::StreamSynchronize();
 
     // Check for nan's
     if (simParams().nan_check)
@@ -178,6 +179,7 @@ void ScalarFieldLevel::specificEvalRHS(amrex::MultiFab &a_soln,
                            TraceARemoval()(cell);
                            PositiveChiAndAlpha(simParams().min_chi, simParams().min_lapse)(cell);
                        });
+    Gpu::StreamSynchronize();
 
     // Calculate MatterCCZ4 right hand side with matter_t = ScalarField
     Potential potential(simParams().potential_params);
@@ -245,6 +247,7 @@ void ScalarFieldLevel::specificUpdateODE(amrex::MultiFab &a_soln)
                                soln_arrs[box_no].cellData(i, j, k);
                            TraceARemoval()(cell);
                        });
+    Gpu::StreamSynchronize();
 }
 
 void ScalarFieldLevel::preTagCells()
