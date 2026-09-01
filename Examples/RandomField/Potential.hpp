3b81c679406d7f6ecfd1b953d25bd5a8eab35ac7
Ericka Florio <eaf49@cam.ac.uk>
2026-09-01 14:54:08 +0100

    Add all RandomField files from branch enhancement/re-organising-RandomFields-class

---
+++ Examples/RandomField/Potential.hpp
@@ -0,0 +1,288 @@
+/* GRTeclyn
+ * Copyright 2022 The GRTL collaboration.
+ * Please refer to LICENSE in GRTeclyn's root directory.
+ */
+
+#ifndef POTENTIAL_HPP_
+#define POTENTIAL_HPP_
+
+#include "simd.hpp"
+#include <typeinfo>
+
+class Potential
+{
+  public:
+    struct params_t
+    {
+		int type;
+		amrex::Real param1, param2, param3, param4, param5;
+
+		// Monodromy parameters
+		amrex::Real scalar_mass = 0.;
+		amrex::Real location;
+		amrex::Real width;	
+		amrex::Real amplitude;
+		amrex::Real period;
+
+		// USR (Prokopec) parameters
+		amrex::Real Lambda;
+		amrex::Real v = 0.;
+
+		// Punctuated inflation params
+		int n;
+		amrex::Real lambda;
+		// mass
+
+		// Quadratic with Gaussian feature (quadbump) parameters
+		// location
+		// amp
+		// width
+    };
+
+  private:
+    params_t m_params;
+
+  public:
+    //! The constructor
+    Potential(params_t a_params) : m_params(a_params) 
+	{
+		switch (m_params.type)
+		{
+			case 1:
+				m_params.scalar_mass = m_params.param1;
+				break;
+
+			case 4:
+				m_params.scalar_mass = m_params.param1;
+				m_params.location = m_params.param2;
+				m_params.amplitude = m_params.param3;
+				m_params.width = m_params.param4;
+				break;
+
+			case 8:
+				m_params.Lambda = m_params.param1;
+				m_params.v = m_params.param2;
+				break;
+			
+			case 9:
+				m_params.scalar_mass = m_params.param1;
+				m_params.location = m_params.param2;
+				m_params.width = m_params.param3;
+				m_params.amplitude = m_params.param4;
+				m_params.period = m_params.param5;
+				break;
+
+			case 10:
+				m_params.scalar_mass = m_params.param1;
+				m_params.n = m_params.param2;
+				m_params.lambda = m_params.param3;
+				break;
+
+			case 11:
+				m_params.scalar_mass = m_params.param1;
+				m_params.location = m_params.param2;
+				m_params.amplitude = m_params.param3;
+				m_params.width = m_params.param4;
+				break;
+
+			case 12:
+				m_params.scalar_mass = m_params.param1;
+				m_params.amplitude = m_params.param2;   // step amplitude c
+				m_params.location = m_params.param3;    // step location phi_s
+				m_params.width = m_params.param4;       // step width d
+				break;
+
+			default:
+				amrex::Print() << m_params.type << ", ";
+				amrex::Print() << typeid(m_params.type).name() << "\n";
+				amrex::Error("Potential::Potential, provided potential "
+							 "type is not implemented");
+		}
+	}
+
+	// Classic quadratic potenital
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	quadratic(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.scalar_mass != 0,
+			"Potential::quadratic, scalar mass is un-initialised");
+
+		V = std::pow(m_params.scalar_mass * phi, 2.) / 2.;
+		dV = std::pow(m_params.scalar_mass, 2.) * phi;
+	}
+
+	// Classic quadratic potenital
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	quadratic_bump(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.scalar_mass != 0,
+			"Potential::quadratic, scalar mass is un-initialised");
+
+		amrex::Real feature = m_params.amplitude * std::exp(
+							-std::pow((phi - m_params.location) / m_params.width, 2.) 
+							/ 2.0);
+		
+		V = std::pow(m_params.scalar_mass * phi, 2.) * (1.0 + feature) / 2.;
+		dV = std::pow(m_params.scalar_mass, 2.) * (phi * (1.0 + feature)
+			 - (std::pow(phi, 2.0) * (phi - m_params.location) * feature 
+			   / std::pow(m_params.width, 2.0) / 4.0));
+	}
+
+	// Monodromy potential, as used in STOIIC and also in arXiv:2403.12811
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	monodromy(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.scalar_mass != 0,
+			"Potential::monodromy, scalar mass is un-initialised");
+
+		// Calculate V
+		amrex::Real argument = (phi - m_params.location)/m_params.period;
+		amrex::Real displaced_argument = (m_params.location - phi + m_params.width)/m_params.period;
+		
+		amrex::Real envelope = 0.25 * (1. + tanh(argument)) * (1. + tanh(displaced_argument));
+		amrex::Real oscillation = cos(argument) - 1.; 	
+
+		V = 0.5 * pow(m_params.scalar_mass * phi, 2.0);
+		V += m_params.amplitude * (envelope * oscillation);
+
+		// Calculate dV
+		amrex::Real d_envelope = 0.25/m_params.period * 
+					((1. + tanh(argument)) * (std::pow(tanh(displaced_argument), 2.) - 1.)
+					+ (1. + tanh(displaced_argument)) * (1. - std::pow(tanh(argument), 2.)));
+		amrex::Real d_oscillation = -sin(argument)/m_params.period;
+
+		dV = pow(m_params.scalar_mass, 2.0) * phi;
+		dV += m_params.amplitude * (envelope * d_oscillation + d_envelope * oscillation);
+	}
+
+	// Prokopec USR model, from arXiv:2507.04114
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	USR(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.v != 0,
+			"Potential::USR, USR parameter v is un-initialised");
+
+		// Calculate V
+		amrex::Real fraction = (3. * pow(phi, 2.) 
+						 + 2. * sqrt(2.) * phi * m_params.v 
+						 + 6. * pow(m_params.v, 2.));
+
+		fraction /= pow(3. * pow(phi, 2.) + 2. * pow(m_params.v, 2.), 2.);
+		V = m_params.Lambda * pow(m_params.v, 4.) * pow(phi, 2.) * fraction / 3.;
+
+		// Calculate dV
+		fraction = ((2. * m_params.v + sqrt(2.) * phi) 
+				  * (pow(phi, 2.) - 2. * pow(m_params.v, 2.)));
+		fraction /= pow(2. * pow(m_params.v, 2.) + 3. * pow(phi, 2.), 3.);
+		dV = - 2. * m_params.Lambda * pow(m_params.v, 5.) * phi * fraction;
+	}
+
+	// Prokopec USR model, from arxiv:0809.3915v2
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	punctuated(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
+			m_params.scalar_mass != 0 || m_params.lambda != 0,
+			"Potential::punctuated, punctuated inflation parameters uninitialised");
+
+		// Calculate V
+		V = pow(m_params.scalar_mass * phi, 2.) / 2.;
+		V += m_params.lambda * pow(phi, 2 * (m_params.n - 1)) / 4.;
+		V -= sqrt(2. * m_params.lambda * (m_params.n - 1)) * m_params.scalar_mass * pow(phi, m_params.n) / m_params.n;
+
+		// Calculate dV
+		dV = pow(m_params.scalar_mass, 2.) * phi;
+		dV += m_params.lambda * (m_params.n - 1) * pow(phi, 2 * m_params.n - 3) / 2.;
+		dV -= sqrt(2. * m_params.lambda * (m_params.n - 1)) * m_params.scalar_mass * pow(phi, m_params.n - 1);
+	}
+
+	// Classic quadratic potenital
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	inverted_quadratic_bump(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.scalar_mass != 0,
+			"Potential::quadratic, scalar mass is un-initialised");
+
+		amrex::Real feature = m_params.amplitude * std::exp(
+							-std::pow((phi - m_params.location) / m_params.width, 2.) 
+							/ 2.0);
+		
+		V = std::pow(m_params.scalar_mass * phi, 2.) * (1.0 - feature) / 2.;
+		dV = std::pow(m_params.scalar_mass, 2.) * (phi * (1.0 - feature)
+			 + (std::pow(phi, 2.0) * (phi - m_params.location) * feature 
+			   / std::pow(m_params.width, 2.0) / 2.0));
+	}
+
+	// Quadratic potential modulated by a tanh step, as used in STOIIC
+	// V = 0.5 m^2 phi^2 [1 + c tanh((phi - phi_s) / d)]
+	template <class data_t>
+	AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+	quadratic_step(data_t &V, data_t &dV, const data_t &phi) const
+	{
+		AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_params.scalar_mass != 0,
+			"Potential::quadratic_step, scalar mass is un-initialised");
+
+		amrex::Real step = tanh((phi - m_params.location) / m_params.width);
+		amrex::Real d_step = (1.0 - std::pow(step, 2.)) / m_params.width;
+
+		V = std::pow(m_params.scalar_mass * phi, 2.)
+			* (1.0 + m_params.amplitude * step) / 2.;
+		dV = std::pow(m_params.scalar_mass, 2.) * (phi * (1.0 + m_params.amplitude * step)
+			 + std::pow(phi, 2.) * m_params.amplitude * d_step / 2.);
+	}
+
+    //! Set the potential function for the scalar field here
+    template <class data_t, template <typename> class vars_t>
+    AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+    compute_potential(data_t &V_of_phi, data_t &dVdphi,
+                      const vars_t<data_t> &vars) const
+    {
+		switch (m_params.type)
+		{
+			case 1:
+				quadratic<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+			
+			case 4:
+				quadratic_bump<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+
+			case 8:
+				USR<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+
+			case 9:
+				monodromy<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+
+			case 10:
+				punctuated<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+
+			case 11:
+				inverted_quadratic_bump<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+
+			case 12:
+				quadratic_step<data_t>(V_of_phi, dVdphi, vars.phi);
+				break;
+			
+			default:
+				AMREX_ALWAYS_ASSERT_WITH_MESSAGE(false,
+					"Potential::compute_potential, "
+					"requested potential type is not supported");
+		}
+    
+		/* amrex::Print().SetPrecision(15) << "V: " << V_of_phi << "\n";
+		amrex::Print().SetPrecision(15) << "dV: " << dVdphi << "\n";
+		amrex::Error(); */
+    }
+};
+
+#endif /* POTENTIAL_HPP_ */
---
+++ Examples/RandomField/TensorTests.hpp
@@ -0,0 +1,191 @@
+/* GRTeclyn
+ * Copyright 2022 The GRTL collaboration.
+ * Please refer to LICENSE in GRTeclyn's root directory.
+ */
+
+#ifndef TENSORTESTS_HPP_
+#define TENSORTESTS_HPP_
+
+#include "InflationUtils.hpp"
+
+#include <AMReX_MultiFab.H>
+#include <AMReX_Reduce.H>
+#include <AMReX_ParallelDescriptor.H>
+#include <AMReX_Print.H>
+#include <AMReX_Math.H>
+
+namespace TensorTests
+{
+    // Test that the input tensor field (config space) is trace free (global)
+    inline void Test_is_trace_free(amrex::MultiFab &field)
+    {
+        if (field.nComp() != 6)
+        {
+            amrex::Error("RandomField::Test_is_trace_free, "
+                         "input field is not a tensor field");
+        }
+
+        const auto &arrs = field.arrays();
+        amrex::ParallelFor(field,
+            [=] AMREX_GPU_DEVICE (int bx, int i, int j, int k)
+            {
+                amrex::Real sum = 0.;
+
+                for(int l=0; l<3; l++)
+                {
+                    sum += arrs[bx](i, j, k, InflationUtils::lut[l][l]);
+                }
+
+                AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
+                    amrex::Math::abs(sum) <= InflationUtils::tolerance,
+                    "TensorTests::Test_is_trace_free, trace-free test failed");
+            }
+        );
+
+        amrex::Gpu::streamSynchronize();
+    }
+
+    // Test that the input vectors are orthonormal (local)
+    inline void Test_vector_orthonorm(const amrex::IntVect iv,
+                                      const amrex::GpuArray<amrex::Real, 3> mhat,
+                                      const amrex::GpuArray<amrex::Real, 3> nhat)
+    {
+        // Confirm basis vectors are orthonormal
+        if (iv != amrex::IntVect{0, 0, 0})
+        {
+            amrex::Real dot1 = 0.;
+            amrex::Real dot2 = 0.;
+            amrex::Real cross1 = 0.;
+            for(int l=0; l<3; l++)
+            {
+                dot1 += mhat[l] * mhat[l];
+                dot2 += nhat[l] * nhat[l];
+                cross1 += mhat[l] * nhat[l];
+            }
+
+            if (std::abs(dot1 - 1.) > InflationUtils::tolerance 
+              || std::abs(dot2 - 1.) > InflationUtils::tolerance 
+              || cross1 > InflationUtils::tolerance)
+            {
+                amrex::Print() << "Location: " << iv << "\n";
+                amrex::Print() << "Dot products: " << dot1 << ", " << dot2 << "\n";
+                amrex::Print() << "Cross product: " << cross1 << "\n";
+                amrex::Print() << "Vectors: \n";
+                for(int l=0; l<3; l++)
+                {
+                    amrex::Print() << l << ", " << mhat[l] << ", " << nhat[l] << "\n";
+                }
+                amrex::Error("RandomField::Test_vector_orthonorm: "
+                             "Basis vectors are not orthonormal here");
+            }
+        }
+    }
+
+    // Test that the input basis tensors, and their rotated counterparts, are orthonormal
+    inline void Test_polarisation_tensor_orthonorm(const amrex::IntVect iv,
+                                                   const Tensor<2, amrex::Real> eplus,
+                                                   const Tensor<2, amrex::Real> ecross)
+    {
+        amrex::GpuArray<amrex::Real, 3> conds;
+        conds.fill(0.);
+
+        for (int l=0; l<3; l++) for (int p=0; p<3; p++)
+        {
+            conds[0] += eplus[l][p] * eplus[l][p];
+            conds[1] += eplus[l][p] * ecross[l][p];
+            conds[2] += ecross[l][p] * ecross[l][p];
+        }
+
+        if (iv != amrex::IntVect{0, 0, 0})
+        {
+            bool plc = (std::abs(conds[0] / 2. - 1.) > InflationUtils::tolerance 
+                        || std::abs(conds[2] / 2. - 1.) > InflationUtils::tolerance);
+            bool ppc = (std::abs(conds[1]) > InflationUtils::tolerance);
+            if (plc || ppc)
+            {
+                amrex::Print() << "---------\nLocation: " << iv << "\n"
+                               << "Dot products: " << conds[0] / 2. << ", "
+                               << conds[2] / 2. << "\n"
+                               << "Cross product: " << conds[1] << "\n"
+                               << "Base tensor components: \n";
+                for (int l=0; l<3; l++) for (int p=0; p<3; p++)
+                {
+                    amrex::Print() << l << ", " << p << ": "
+                                   << eplus[l][p] << ", " << ecross[l][p] << "\n";
+                }
+                amrex::Error("RandomField::Test_polarisation_tensor_orthonorm: "
+                             "polarisation tensors are not orthonormal here");
+            }
+        }
+    }
+
+    // Sum the total power |f_k|^2 over Fourier space, accounting for the
+    // Hermitian modes not stored explicitly (factor of 2 in the bulk)
+    inline amrex::Real calculate_total_power(const amrex::cMultiFab &fk, const int N)
+    {
+        // 1. Set up the parallel reduction operation (Sum)
+        amrex::ReduceOps<amrex::ReduceOpSum> reduce_op;
+        amrex::ReduceData<amrex::Real> reduce_data(reduce_op);
+        using ReduceTuple = typename decltype(reduce_data)::Type;
+
+        // 2. Loop over the grids owned by this MPI rank
+        for (amrex::MFIter mfi(fk); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box &bx = mfi.fabbox();
+            auto const &arr = fk.const_array(mfi);
+
+            // 3. Execute the parallel reduction on the CPU/GPU
+            reduce_op.eval(bx, reduce_data,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k) -> ReduceTuple
+                {
+                    // Get the real and imaginary parts
+                    amrex::Real re = arr(i, j, k).real();
+                    amrex::Real im = arr(i, j, k).imag();
+
+                    amrex::Real pow = re * re + im * im;
+                    // Multiply by 2 in most of the bulk,
+                    // to account for Hermitian modes.
+                    if (i != 0 && i != N/2) { pow *= 2.; }
+
+                    return pow;
+                });
+        }
+
+        // 4. Extract the aggregated sum for this specific MPI rank
+        ReduceTuple hv = reduce_data.value();
+        amrex::Real total_local_power = amrex::get<0>(hv);
+
+        // 5. Perform an MPI reduction to sum across all compute nodes
+        amrex::ParallelDescriptor::ReduceRealSum(total_local_power);
+
+        return total_local_power;
+    }
+
+    // Confirm Parseval's theorem holds between a config-space field hx and its
+    // Fourier-space counterpart hk (checked before physical normalisation)
+    inline void Test_Parsevals_thm(const amrex::MultiFab &hx,
+                                   const amrex::cMultiFab &hk, const int N)
+    {
+        amrex::Real xsum = std::pow(hx.norm2(), 2.);
+        xsum /= std::pow(N, 3.);
+
+        amrex::Real ksum = calculate_total_power(hk, N);
+
+        int p = std::round(std::log10((ksum + ksum) / 2.));
+        amrex::Real tol = InflationUtils::tolerance * std::pow(10., p+1);
+
+        if (std::abs(xsum - ksum) > tol)
+        {
+            amrex::Print() << "Order of magnitude of sum: " << p << "\n";
+            amrex::Print() << "Tolerance: " << tol << "\n";
+            amrex::Print() << "Stdev (x): " << xsum << "\n";
+            amrex::Print() << "Integrated power (k): " << ksum << "\n";
+            amrex::Print() << "Ratio: " << ksum / xsum << "\n";
+            amrex::Print() << "Difference: " << std::abs(ksum - xsum) << "\n";
+            amrex::Error("TensorTests::Test_Parsevals_thm, "
+                         "Parseval's theorem fails here.");
+        }
+    }
+}
+
+#endif /* TENSORTESTS_HPP_ */
