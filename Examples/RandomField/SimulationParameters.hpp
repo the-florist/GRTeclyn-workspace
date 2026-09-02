e1c87f8799a8d59745b43e45bd07d25f7c3c45b2
Training Account <z303@login-s-1.data.cluster>
2026-09-02 12:16:25 +0100

    Add SimulationParameters (needed in Source) and make into as bare a class as possible

---
+++ Examples/RandomField/SimulationParameters.hpp
@@ -0,0 +1,23 @@
+/* GRTeclyn
+ * Copyright 2022 The GRTL collaboration.
+ * Please refer to LICENSE in GRTeclyn's root directory.
+ */
+
+#ifndef SIMULATIONPARAMETERS_HPP_
+#define SIMULATIONPARAMETERS_HPP_
+
+#include "BaseParameterChecker.hpp"
+#include "CCZ4RHS.hpp"
+#include "FixedGridsTagger.hpp"
+#include "IntegratedMovingPunctureGauge.hpp"
+#include "LineExtractionParameters.hpp"
+#include "Potential.hpp"
+#include "ScalarField.hpp"
+
+class SimulationParameters
+{
+  public:
+    SimulationParameters() = delete;
+};
+
+#endif /* SIMULATIONPARAMETERS_HPP_ */
