5d5c268e69805f60de4ef4a1adb10f227e2c4f4f
Ericka Florio <eaf49@cam.ac.uk>
2026-09-02 10:36:26 +0100

    Rewrite RandomFieldInit in the new style

--- Examples/RandomField/RandomFieldInit.hpp
+++ Examples/RandomField/RandomFieldInit.hpp
@@ -6,9 +6,9 @@
 #ifndef RANDOMFIELDINIT_HPP_
 #define RANDOMFIELDINIT_HPP_
 
+#include "GRParmParse.hpp"
 #include "InflationConfig.hpp"
-#include "InitialBackgroundData.hpp"
-#include "Potential.hpp"
+#include "StateVariables.hpp"
 #include "TensorTests.hpp"
 
 #include <AMReX_FFT.H>
@@ -17,57 +17,14 @@
 class RandomFieldInit
 {
     protected:
-        amrex::Real H0;
-        amrex::Real phi0;
-
-        static amrex::Real
-        calc_H0(amrex::Real G, amrex::Real Pi, amrex::Real V)
-        {
-            return sqrt((8. * amrex::Math::pi<amrex::Real>() * G / 3.)
-                        * (0.5 * pow(Pi, 2.) + V));
-        }
+        InflationConfig inflt_methods;
 
     public:
-                // Constructor used when initialising stochastic fields
-        RandomFieldInit(const InflationConfig a_config,
-                        const InitialBackgroundData::params_t bkgd_params,
-                        const Potential::params_t potential_params)
-                        : m_params(a_config)
+        // Constructor used when initialising stochastic fields
+        RandomFieldInit()
         {
-            // Compute background potential
-            amrex::Real V, dV;
-            Potential potential(potential_params);
-            switch (potential_params.type)
-            {
-                case 1:
-                    potential.quadratic(V, dV, bkgd_params.phi0);
-                    break;
-                case 4:
-                    potential.quadratic_bump(V, dV, bkgd_params.phi0);
-                    break;
-                case 8:
-                    potential.USR(V, dV, bkgd_params.phi0);
-                    break;
-                case 9:
-                    potential.monodromy(V, dV, bkgd_params.phi0);
-                    break;
-                case 10:
-                    potential.punctuated(V, dV, bkgd_params.phi0);
-                    break;
-                case 11:
-                    potential.inverted_quadratic_bump(V, dV, bkgd_params.phi0);
-                    break;
-                case 12:
-                    potential.quadratic_step(V, dV, bkgd_params.phi0);
-                    break;
-                default:
-                    amrex::Error("RandomFieldInit::RandomFieldInit,"
-                                 "potential type not provided");
-            }
-
-            // Compute initial Hubble parameter
-            H0 = calc_H0(bkgd_params.G_Newton, bkgd_params.Pi0, V);
-            phi0 = bkgd_params.phi0;
+            // Read the parameters once from the global GRParmParse table
+            inflt_methods.fill_params();
 
             // Flatten and upload the STOIIC spectra to the device
             upload_stoiic_to_device();
@@ -80,9 +37,7 @@ class RandomFieldInit
         enum class ScalarField { Phi = 0, Pi = 1, Chi = 2, K = 3 };
         enum class TensorField { Amplitude = 0, Velocity = 1 };
 
-        InflationConfig m_params;
-
-        // Device storage backing the STOIIC pointers in m_params
+        // Device storage backing the STOIIC pointers in inflt_methods
         amrex::Gpu::DeviceVector<amrex::Real> m_init_k_d;
         amrex::Gpu::DeviceVector<amrex::Real> m_scalar_ps_d;
         amrex::Gpu::DeviceVector<amrex::Real> m_tensor_ps_d;
@@ -90,17 +45,17 @@ class RandomFieldInit
         void upload_stoiic_to_device();
 
         AMREX_GPU_HOST_DEVICE static amrex::GpuComplex<amrex::Real>
-        calculate_mode_function(const InflationParams &cfg, const amrex::Real H0,
+        calculate_mode_function(const InflationConfig &cfg,
                                 const amrex::Real km,
                                 const TensorField field_selector);
 
         AMREX_GPU_HOST_DEVICE static amrex::GpuComplex<amrex::Real>
-        find_in_stoiic(const InflationParams &cfg, const amrex::Real km,
+        find_in_stoiic(const InflationConfig &cfg, const amrex::Real km,
                        const FieldType field_type,
                        const auto field_selector);
 
         AMREX_GPU_HOST_DEVICE static amrex::GpuComplex<amrex::Real>
-        calculate_random_field(const InflationParams &cfg, const amrex::Real H0,
+        calculate_random_field(const InflationConfig &cfg,
                                const amrex::IntVect iv,
                                const amrex::Real rand_amp,
                                const amrex::Real rand_phase,
