f6850d3dfb89f684ef6a88c6f8a91d6cf2a63329
Ericka Florio <eaf49@cam.ac.uk>
2026-09-03 11:00:41 +0100

    Comment out inflaton test for now

--- Tests/TestCases.hpp
+++ Tests/TestCases.hpp
@@ -20,8 +20,8 @@
 #include "CoordinateTransformationsTest.hpp"
 #include "DerivativeUnitTests.hpp"
 #include "EMTensorTest.hpp"
-#include "InflatonFieldBackgroundTest.hpp"
-#include "InflatonFieldStatisticsTest.hpp"
+// #include "InflatonFieldBackgroundTest.hpp"
+// #include "InflatonFieldStatisticsTest.hpp"
 #include "ParticleInterpolatorUnitTest.hpp"
 #include "PositiveChiAndLapseUnitTest.hpp"
 #include "PunctureTrackerTest.hpp"
@@ -71,7 +71,7 @@ TEST_CASE("EMTensor"
     run_emtensor_test();
 }
 
-TEST_CASE("InflatonFieldStatistics") { run_inflaton_field_statistics_test(); }
+// TEST_CASE("InflatonFieldStatistics") { run_inflaton_field_statistics_test(); }
 
 TEST_CASE("PositiveChiAndLapse") { run_positive_chi_and_lapse_unit_test(); }
 
