31219501df57bdbe4aa31f7fb14bda1915da6051
Ericka Florio <eaf49@cam.ac.uk>
2026-09-03 11:03:39 +0100

    Remove Inflation test totally (for clang format)

--- Tests/TestCases.hpp
+++ Tests/TestCases.hpp
@@ -20,8 +20,6 @@
 #include "CoordinateTransformationsTest.hpp"
 #include "DerivativeUnitTests.hpp"
 #include "EMTensorTest.hpp"
-// #include "InflatonFieldBackgroundTest.hpp"
-// #include "InflatonFieldStatisticsTest.hpp"
 #include "ParticleInterpolatorUnitTest.hpp"
 #include "PositiveChiAndLapseUnitTest.hpp"
 #include "PunctureTrackerTest.hpp"
@@ -70,8 +68,6 @@ TEST_CASE("EMTensor"
 {
     run_emtensor_test();
 }
-
-// TEST_CASE("InflatonFieldStatistics") { run_inflaton_field_statistics_test(); }
 
 TEST_CASE("PositiveChiAndLapse") { run_positive_chi_and_lapse_unit_test(); }
 
