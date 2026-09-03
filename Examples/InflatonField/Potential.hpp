773106a787e43ef029177317476de75d182cef2a
Ericka Florio <eaf49@cam.ac.uk>
2026-09-03 11:29:58 +0100

    Clang format fixes

--- Examples/InflatonField/Potential.hpp
+++ Examples/InflatonField/Potential.hpp
@@ -74,7 +74,7 @@ class Potential
     // NOLINTBEGIN(bugprone-easily-swappable-parameters)
     AMREX_GPU_DEVICE AMREX_FORCE_INLINE void
     compute_background_potential(amrex::Real &V_of_phi, amrex::Real &dVdphi,
-                      const amrex::Real &phi) const
+                                 const amrex::Real &phi) const
     {
         const amrex::Real mass_times_phi = m_params.scalar_mass * phi;
         V_of_phi = 0.5 * mass_times_phi * mass_times_phi;
